b179266e00620d735fdea7a8875984ea2b485c30
Phoenix1747 <apertured@public-files.de>
2017-12-14 19:53:35 +0100

    cpu function

--- Code/cpu.py
+++ Code/cpu.py
@@ -3,19 +3,21 @@
 #from time import sleep
 from psutil import cpu_percent
 
-pin = 23
+def cpuFunction():
+  
+  pin = 23
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(pin,GPIO.OUT)
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setwarnings(False)
+  GPIO.setup(pin,GPIO.OUT)
 
-while True:
+  while True:
 
-  CPU = cpu_percent()
+    CPU = cpu_percent()
 
-  if CPU > 15:
-    GPIO.output(pin,GPIO.HIGH)
-  else:
-    GPIO.output(pin,GPIO.LOW)
+    if CPU > 15:
+      GPIO.output(pin,GPIO.HIGH)
+    else:
+      GPIO.output(pin,GPIO.LOW)
 
-  sleep(0.5)
+    sleep(0.5)
