9b8f71bdea19919df42fcb320bf977d9767ddf41
Phoenix1747 <apertured@public-files.de>
2017-12-14 19:58:58 +0100

    Update connection.py

--- Code/connection.py
+++ Code/connection.py
@@ -3,28 +3,26 @@
 #from time import sleep
 #from os import system
 
+pin = 22
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pin,GPIO.OUT)
+
+def connection():
+
+  response = system("ping -c 1 8.8.8.8 ")
+
+  if response == 0:
+    return True
+  else:
+    return False
+
 def connectionFunction():
   
-  pin = 22
+  if connection():
+    GPIO.output(pin,GPIO.HIGH)
+  else:
+    GPIO.output(pin,GPIO.LOW)
 
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setwarnings(False)
-  GPIO.setup(pin,GPIO.OUT)
-
-  def connection():
-
-    response = system("ping -c 1 8.8.8.8 ")
-
-    if response == 0:
-      return True
-    else:
-      return False
-
-  while True:
-
-    if connection():
-      GPIO.output(pin,GPIO.HIGH)
-    else:
-      GPIO.output(pin,GPIO.LOW)
-
-    sleep(4)
+  sleep(4)
