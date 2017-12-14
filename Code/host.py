701b14f8c2c8debf2caadff68b3b4aae9c6d0ca8
Phoenix1747 <apertured@public-files.de>
2017-12-14 20:01:01 +0100

    Update host.py

--- Code/host.py
+++ Code/host.py
@@ -3,19 +3,17 @@
 #from time import sleep
 #from subprocess import check_output
 
+pin = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pin,GPIO.OUT)
+
 def hostFunction():
   
-  pin = 24
+  if 'hostapd' in check_output(['ps','-A']):
+    GPIO.output(pin,GPIO.HIGH)
+  else:
+    GPIO.output(pin,GPIO.LOW)
 
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setwarnings(False)
-  GPIO.setup(pin,GPIO.OUT)
-
-  while True:
-
-    if 'hostapd' in check_output(['ps','-A']):
-      GPIO.output(pin,GPIO.HIGH)
-    else:
-      GPIO.output(pin,GPIO.LOW)
-
-    sleep(4)
+  sleep(4)
